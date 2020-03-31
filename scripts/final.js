55bb2c8a46ec5f8cc312488e8f972e699192a0e5
Kapil <ksomani@mathworks.com>
2020-03-31 06:45:12 -0400

    Render rewards for selected store

--- scripts/final.js
+++ scripts/final.js
@@ -88,7 +88,6 @@ class Wallet {
                 });
             }
         }
-
         return selected;
     }
     init() {
@@ -244,15 +243,66 @@ class Util {
     }
     static values(keys, dictionary) {
         var selected = {};
-        for(var key in dictionary) {
-            if (key in dictionary)
+        keys.forEach(key => {
+            if (key in dictionary) {
                 selected[key] = dictionary[key];
-        }
+            }
+        });
         return selected;
+    }
+}
+
+class Render {
+    constructor(selectorId) {
+        this.selectorId = selectorId;
+    }
+    addToHTML(data) {
+        this.clearTableRows();
+        this.addTableRows(data);
+    }
+    clearTableRows() {
+        var table_header = document.getElementById(this.selectorId).getElementsByTagName("thead")[0];
+        document.getElementById(this.selectorId).innerHTML = "";
+        document.getElementById(this.selectorId).appendChild(table_header);
+    }
+    addTableRows(data1) {
+        data1.forEach(data => {
+            var tr = document.createElement('tr');
+            var card_name = document.createElement('th');
+            card_name.innerHTML = data['card_name'];
+            tr.appendChild(card_name);
+            var merchant_type = document.createElement('th');
+            merchant_type.innerHTML = data['merchant_type'];
+            tr.appendChild(merchant_type);
+            var merchant_name = document.createElement('th');
+            merchant_name.innerHTML = data['merchant_name'];
+            tr.appendChild(merchant_name);
+            var reward_type = document.createElement('th');
+            reward_type.innerHTML = data['reward_type'];
+            tr.appendChild(reward_type);
+            var reward_value = document.createElement('th');
+            reward_value.innerHTML = data['reward_value'];
+            tr.appendChild(reward_value);
+            var start_date = document.createElement('th');
+            start_date.innerHTML = data['start_date'];
+            tr.appendChild(start_date);
+            var end_date = document.createElement('th');
+            end_date.innerHTML = data['end_date'];
+            tr.appendChild(end_date);
+            var note = document.createElement('th');
+            note.innerHTML = data['note'];
+            tr.appendChild(note);
+
+            document.getElementById(this.selectorId).appendChild(tr);
+        });
     }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     var wallet = new Wallet();
-    console.log(wallet.getRewards('chee'));
+    var render = new Render("rewards-table");
+    document.getElementById("searchBox").addEventListener("input", function() {
+        render.addToHTML(wallet.getRewards(document.getElementById("searchBox").value));
+    });
+
 })
